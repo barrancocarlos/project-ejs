54651bdd66116779d2eaebe7523a9af924830734
Carlos <barrancocarlos@hotmail.com>
2018-01-11 12:46:02 -0400

    tests

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 var config = require('./config');
 
-mongoose.connect(config.dbhost);
+// avoid mongoose library warning
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.dbhost, {useMongoClient: true,});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
--- test.js
+++ test.js
@@ -12,15 +12,47 @@ var expect = require('chai').expect;
 chai.use(chaiHttp);
 
 // rest api test
-describe('api endpoits', function() {
-
-  it('should list all projects on /api', function(done) {
+describe('/api endpoits', function() {
+  //get all
+  it('should get all projects on /api', function(done) {
     chai.request(config.host + ':' + config.port)
       .get('/api')
       .end(function(err, res) {
         expect(res).to.have.status(200);
+        expect(res).be.a('object');
         done();
       });
   });
+  // create new
+  it('create new project on /api', function(done) {
+    var project = {
+            name: "Mocha",
+            activitie: "Success",
+        };
+    chai.request(config.host + ':' + config.port)
+      .post('/api')
+      .send(project)
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        expect(res).be.a('object');
+        done();
+      });
+  });
+});//end api endpoits
 
-});
+describe('/Get/:id project', function() {
+    it('gets a single project given the id', function(done) {
+      var project = new Project ({
+              name: "delete",
+              activitie: "delete",
+          });
+      project.save(function(err, data) {
+        chai.request(config.host + ':' + config.port)
+          .get('/api/:' + data.id)
+          .end(function(err, res) {
+            expect(res).to.have.status(200);
+            done();
+          });
+      });
+    });
+});//end get :id
